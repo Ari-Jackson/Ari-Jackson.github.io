50dbce79ac6cf216fd5b98d9e08271e5ff28264a
Ari-Jackson <112004676+Ari-Jackson@users.noreply.github.com>
2022-12-20 14:58:52 -0500

    Adds comments to JS for clarity

--- app.js
+++ app.js
@@ -1,69 +1,83 @@
+/* <----------- Selectors -----------> */
 const blinkDisplay = document.querySelector(".text")
 const cursor = document.querySelector(".cursor")
 
-const phrasesLong = ["a web developer.", "in love with Tori." ,"Brooklyn born and raised.", "a lifetime learner.", "a proud dog dad.", "excited to meet you!"]
+/* <----------- Functions -----------> */
+const phrasesLong = ["a web developer.", "Jada's cool awesome amazing big brother." ,"Brooklyn born and raised.", "a lifetime learner.", "a proud dog dad.", "excited to meet you!"]
 
-let i = 0;
-let j = 0;
+function typePhrases(){
+    let i = 0;
+    let j = 0;
 
-let current = [];
-let isDeleting = false;
+    let current = [];
+    let isDeleting = false;
+    let time;
 
-function loop(){
-    let time = Math.random() * (200-80) + 80;
+    function loop(){
+        time = Math.random() * (200-80) + 80;
 
-    if(!cursor.classList.contains("typing")){
-        cursor.classList.add("typing")
-    }
-
-    if(i == phrasesLong.length){
-        i = 0
-    }
-
-    if(i < phrasesLong.length){
-        if(isDeleting == false){
-            if(j <= phrasesLong[i].length){
-                current.push(phrasesLong[i][j])
-                j++
-            }
-        
-            if(j > phrasesLong[i].length){
-                isDeleting = true;
-            }
+        if(!cursor.classList.contains("typing")){
+            cursor.classList.add("typing")
         }
-
-        if(isDeleting == true){
-
-            if(j >= 0){
-                time = 90;
-                current.pop()
-                j--
-            }
-
-            if(j == phrasesLong[i].length){
-                time = 2000;
-                if(cursor.classList.contains("typing")){
-                    cursor.classList.remove("typing")
+    
+        if(i == phrasesLong.length){
+            i = 0
+        }
+    
+        if(i < phrasesLong.length){
+            if(isDeleting == false){
+                if(j <= phrasesLong[i].length){
+                    current.push(phrasesLong[i][j])
+                    j++
+                }
+            
+                if(j > phrasesLong[i].length){
+                    isDeleting = true;
                 }
             }
-
-            if(j == 0){
-                isDeleting = false;
-                j = 0;
-                i++;
-                time = 1000;
-                if(cursor.classList.contains("typing")){
-                    cursor.classList.remove("typing")
+    
+            if(isDeleting == true){
+    
+                if(j >= 0){
+                    time = 70;
+                    current.pop()
+                    j--
+                }
+    
+                if(j == phrasesLong[i].length){
+                    time = 2000;
+                    if(cursor.classList.contains("typing")){
+                        cursor.classList.remove("typing")
+                    }
+                }
+    
+                if(j == 0){
+                    isDeleting = false;
+                    i++;
+                    time = 1000;
+                    if(cursor.classList.contains("typing")){
+                        cursor.classList.remove("typing")
+                    }
                 }
             }
         }
+    
+        blinkDisplay.textContent = current.join("");
+    
+        setTimeout(loop, time)
     }
-
-    blinkDisplay.textContent = current.join("");
-
-    setTimeout(loop, time)
+    loop()
 }
 
+aboutMeObj = {
+    name: "My name is Ari Joseph Kordell Jackson. Which is very long so just call me AJ, thats what everyone but my mom does. (If you're my mom of course do what you gotta do.)",
+    location: "I'm Brooklyn, born and raised. ",
+    passions:"I have too many passions to count, but they all center around my love for learning and learning to love."
+}
+
+
+/* <----------- Event Listeners -----------> */
+
 document.addEventListener("DOMContentLoaded", () => {
-    setTimeout(loop(), 5000)
+    setTimeout(typePhrases, 1000)
 })
